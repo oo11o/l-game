56ca39c718e97cd9cb9b3bc58adb18b119bcb4ee
Volodymyr Yurchenko <discover@.url.net>
2025-05-26 18:38:00 +0300

    init: add migration

---
+++ src/database/migrations/2025_05_25_151407_create_links_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('player_id')->constrained()->onDelete('cascade');
+            $table->uuid('slug')->unique();
+            $table->boolean('is_active')->default(true);
+            $table->timestamp('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('links');
+    }
+};
